cecb31ec6cf328c34a77ef06b940992b957cf79f
jovian <jovian@momnpop.asia>
2025-09-27 06:38:23 +0700

    reverse string and GCD

---
+++ app/CompetitiveProgramming/EuclideanAlgorithm.cpp
@@ -0,0 +1,27 @@
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+// bitwise operator
+// bool isPowerOfTwo(int n) {
+//     return n > 0 && (n & (n - 1)) == 0;
+// }
+
+bool isPowerOfTwo(int n) {
+    if(n == 1) return true;
+
+    if(n <= 0 || n % 2 != 0) return false;
+
+    return isPowerOfTwo(n/2);
+}
+
+int main() {
+   
+    
+    std::cout << isPowerOfTwo(20) << std::endl;
+   
+
+    return 0;
+}
